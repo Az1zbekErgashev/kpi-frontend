f261042e72be87004f4effcba164cd6ae07bb4e6
Az1zbekErgashev <ergasevaziz0611@gmail.com>
2025-08-01 15:17:44 +0900

    feat: Add file upload functionality to UsersFilter component with success/error messages

--- src/components/User/UserFilter/index.tsx
+++ src/components/User/UserFilter/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { StyledUsersFilter } from './style';
-import { Form } from 'antd';
+import { Form, Upload, message } from 'antd';
 import { Button, Input } from 'ui';
 import { useTranslation } from 'react-i18next';
+import { UploadOutlined } from '@ant-design/icons';
+import { routes } from 'config/config';
+import useJwt from 'utils/useJwt';
 
 interface props {
   handleValueChange: any;
@@ -19,17 +22,42 @@ interface props {
 export function UsersFilter({ handleValueChange, setActionModalConfig }: props) {
   const { t } = useTranslation();
   const [form] = Form.useForm();
+  const { getHeader } = useJwt();
+  const handleUploadChange = (info: any) => {
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} ${t('uploaded_successfully')}`);
+      window.location.reload(); // перезагрузка после успешной загрузки
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} ${t('upload_failed')}`);
+    }
+  };
+
   return (
     <StyledUsersFilter>
       <Form form={form} layout="vertical" onValuesChange={handleValueChange}>
         <Input allowClear placeholder={t('search....')} name="text" label={t('search_by_username_fullname_team')} />
       </Form>
 
-      <Button
-        label={t('create_user')}
-        type="primary"
-        onClick={() => setActionModalConfig({ open: true, title: 'add_user', type: 'ADD' })}
-      />
+      <div style={{ display: 'flex', gap: 8 }}>
+        <Button
+          label={t('create_user')}
+          type="primary"
+          onClick={() => setActionModalConfig({ open: true, title: 'add_user', type: 'ADD' })}
+        />
+
+        <Upload
+          name="file"
+          accept=".xlsx,.xls"
+          showUploadList={false}
+          action={`${routes.api.baseUrl}/api/user`}
+          onChange={handleUploadChange}
+          headers={{
+            Authorization: getHeader(),
+          }}
+        >
+          <Button icon={<UploadOutlined />} label={t('upload_excel')} />
+        </Upload>
+      </div>
     </StyledUsersFilter>
   );
 }
